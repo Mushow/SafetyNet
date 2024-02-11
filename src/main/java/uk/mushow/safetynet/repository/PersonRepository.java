8b42cf14bcd5fa475d44bca4cefa9fce4081f72d
Mushow <antoine@mushow.uk>
2024-02-11 22:29:31 +0100

    Refactor PersonRepository to Use DataWrapper for Data Management and Exception Handling

--- src/main/java/uk/mushow/safetynet/repository/PersonRepository.java
+++ src/main/java/uk/mushow/safetynet/repository/PersonRepository.java
@@ -1,20 +1,48 @@
 package uk.mushow.safetynet.repository;
 
 import org.springframework.stereotype.Repository;
+import uk.mushow.safetynet.data.DataWrapper;
+import uk.mushow.safetynet.exceptions.PersonNotFoundException;
 import uk.mushow.safetynet.model.Person;
 
 @Repository
 public class PersonRepository {
 
-    public Person create(Person person) {
-        return null;
+    private final DataWrapper dataWrapper;
+
+    public PersonRepository(DataWrapper dataWrapper) {
+        this.dataWrapper = dataWrapper;
     }
 
-    public Person update(Person person) {
-        return null;
+    public Person create(Person person) {
+        dataWrapper.persons().add(person);
+        return person;
+    }
+
+    public Person update(Person updatedPerson) {
+        String id = updatedPerson.getFirstName() + updatedPerson.getLastName();
+        for (Person currentPerson : dataWrapper.persons()) {
+            if ((currentPerson.getFirstName() + currentPerson.getLastName()).equals(id) ) {
+                updatePersonInformation(currentPerson, updatedPerson);
+                return currentPerson;
+            }
+        }
+        throw new PersonNotFoundException("The person: " + updatedPerson.getFirstName() + " " + updatedPerson.getLastName() + "was not found!");
     }
 
     public void delete(String firstName, String lastName) {
+        boolean wasRemoved = dataWrapper.persons().removeIf(person ->
+                person.getFirstName().equals(firstName) && person.getLastName().equals(lastName));
+
+        if (!wasRemoved) throw new PersonNotFoundException("The person: " + firstName + " " + lastName + "was not found!");
+    }
+
+    private void updatePersonInformation(Person current, Person update) {
+        current.setAddress(update.getAddress());
+        current.setCity(update.getCity());
+        current.setZip(update.getZip());
+        current.setEmail(update.getEmail());
+        current.setPhone(update.getPhone());
     }
 
 }
