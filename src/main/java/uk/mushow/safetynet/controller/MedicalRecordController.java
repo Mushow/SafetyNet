505c9511d396cf27740ab8630d7950e1d9871a79
Mushow <antoine@mushow.uk>
2024-02-11 23:35:41 +0100

    Add MedicalRecordController

---
+++ src/main/java/uk/mushow/safetynet/controller/MedicalRecordController.java
@@ -0,0 +1,9 @@
+package uk.mushow.safetynet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicalRecord")
+public class MedicalRecordController {
+}
