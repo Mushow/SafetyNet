affd7b1fbef3e559a84bbf0c86004fd2f1feea6b
Mushow <antoine@mushow.uk>
2024-02-11 21:02:52 +0100

    Refactor DataWrapper class to a record

--- src/main/java/uk/mushow/safetynet/data/DataWrapper.java
+++ src/main/java/uk/mushow/safetynet/data/DataWrapper.java
@@ -1,17 +1,12 @@
 package uk.mushow.safetynet.data;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import uk.mushow.safetynet.model.Firestation;
 import uk.mushow.safetynet.model.MedicalRecord;
 import uk.mushow.safetynet.model.Person;
 
 import java.util.List;
 
-@Data
-public class DataWrapper {
 
-    private final List<Person> persons;
-    private final List<Firestation> firestations;
-    private final List<MedicalRecord> medicalRecords;
-
-}
+public record DataWrapper(List<Person> persons, List<Firestation> firestations, List<MedicalRecord> medicalRecords) {}
