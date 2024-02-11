077a57286ae4a48093b197f9b1e6f729aeb8d971
Mushow <antoine@mushow.uk>
2024-02-11 23:35:34 +0100

    Add FirestationController

---
+++ src/main/java/uk/mushow/safetynet/controller/FirestationController.java
@@ -0,0 +1,10 @@
+package uk.mushow.safetynet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/firestation")
+public class FirestationController {
+
+}
