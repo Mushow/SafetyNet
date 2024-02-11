0bb77b1bb94383504a0596b3a0098dc5caa4933e
Mushow <antoine@mushow.uk>
2024-02-11 22:28:34 +0100

    Add @Component

--- src/main/java/uk/mushow/safetynet/data/DataWrapper.java
+++ src/main/java/uk/mushow/safetynet/data/DataWrapper.java
@@ -1,9 +1,11 @@
 package uk.mushow.safetynet.data;
 
+import org.springframework.stereotype.Component;
 import uk.mushow.safetynet.model.Firestation;
 import uk.mushow.safetynet.model.MedicalRecord;
 import uk.mushow.safetynet.model.Person;
 
 import java.util.List;
 
+@Component
 public record DataWrapper(List<Person> persons, List<Firestation> firestations, List<MedicalRecord> medicalRecords) {}
