23332bb749981f2bdb941954a5bc2a2dc553b95b
Mushow <antoine@mushow.uk>
2024-02-11 21:08:31 +0100

    Optimize imports

--- src/main/java/uk/mushow/safetynet/data/DataWrapper.java
+++ src/main/java/uk/mushow/safetynet/data/DataWrapper.java
@@ -1,12 +1,9 @@
 package uk.mushow.safetynet.data;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import uk.mushow.safetynet.model.Firestation;
 import uk.mushow.safetynet.model.MedicalRecord;
 import uk.mushow.safetynet.model.Person;
 
 import java.util.List;
-
 
 public record DataWrapper(List<Person> persons, List<Firestation> firestations, List<MedicalRecord> medicalRecords) {}
