8e37f1bf3965a2a27361879050c49f04cf9c8e4f
Mushow <antoine@mushow.uk>
2024-02-12 01:35:30 +0100

    Created StationNotFoundException

---
+++ src/main/java/uk/mushow/safetynet/exception/StationNotFoundException.java
@@ -0,0 +1,9 @@
+package uk.mushow.safetynet.exception;
+
+public class StationNotFoundException extends Exception {
+
+    public StationNotFoundException(String message) {
+        super(message);
+    }
+
+}
