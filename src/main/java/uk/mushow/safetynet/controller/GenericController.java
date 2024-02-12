98d2be2015502e0a342760a84c8c1c204850df03
Mushow <antoine@mushow.uk>
2024-02-12 22:19:51 +0100

    Add getChildAlert GET mapping

--- src/main/java/uk/mushow/safetynet/controller/GenericController.java
+++ src/main/java/uk/mushow/safetynet/controller/GenericController.java
@@ -1,12 +1,25 @@
 package uk.mushow.safetynet.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import uk.mushow.safetynet.dto.ChildDTO;
+import uk.mushow.safetynet.service.FirestationService;
+import uk.mushow.safetynet.service.PersonService;
 
 @RestController
 @RequestMapping("/")
 public class GenericController {
 
+    private final PersonService personService;
+    GenericController(PersonService personService) {
+        this.personService = personService;
+    }
 
+    @GetMapping("/childAlert")
+    public ChildDTO getChildAlert(@RequestParam("address") String address) {
+        return personService.getChildAlertByAddress(address);
+    }
 
 }
