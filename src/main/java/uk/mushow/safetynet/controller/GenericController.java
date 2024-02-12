de4cb185d267d59c83f1a059ccab9a42b21e53f1
Mushow <antoine@mushow.uk>
2024-02-12 23:11:48 +0100

    Add phoneAlert GET request

--- src/main/java/uk/mushow/safetynet/controller/GenericController.java
+++ src/main/java/uk/mushow/safetynet/controller/GenericController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.mushow.safetynet.dto.ChildDTO;
+import uk.mushow.safetynet.dto.PhoneAlertDTO;
+import uk.mushow.safetynet.service.FirestationService;
 import uk.mushow.safetynet.service.PersonService;
 
 import java.util.List;
@@ -14,13 +16,21 @@ import java.util.List;
 public class GenericController {
 
     private final PersonService personService;
-    GenericController(PersonService personService) {
+    private final FirestationService firestationService;
+
+    GenericController(PersonService personService, FirestationService firestationService) {
         this.personService = personService;
+        this.firestationService = firestationService;
     }
 
     @GetMapping("/childAlert")
     public List<ChildDTO> getChildAlert(@RequestParam("address") String address) {
         return personService.getChildAlertByAddress(address);
+    }
+
+    @GetMapping("phoneAlert")
+    public PhoneAlertDTO getPhoneAlert(@RequestParam("firestation") int stationNumber) {
+        return firestationService.getPhoneAlertByStationNumber(stationNumber);
     }
 
 }
