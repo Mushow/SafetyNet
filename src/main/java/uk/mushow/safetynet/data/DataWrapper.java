a318628b3303bfd0a4dca27f18e7928e5c19f0de
Mushow <antoine@mushow.uk>
2024-02-12 14:55:34 +0100

    Change DataWrapper record to a class with @Setter & @Getter to populate data from JsonDataLoader and allow Spring Dependency Injection

--- src/main/java/uk/mushow/safetynet/data/DataWrapper.java
+++ src/main/java/uk/mushow/safetynet/data/DataWrapper.java
@@ -1,11 +1,20 @@
 package uk.mushow.safetynet.data;
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 import uk.mushow.safetynet.model.Firestation;
 import uk.mushow.safetynet.model.MedicalRecord;
 import uk.mushow.safetynet.model.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public record DataWrapper(List<Person> persons, List<Firestation> firestations, List<MedicalRecord> medicalRecords) {}
+@Data
+public class DataWrapper {
+
+    private List<Person> persons = new ArrayList<>();
+    private List<Firestation> firestations = new ArrayList<>();
+    private List<MedicalRecord> medicalRecords = new ArrayList<>();
+
+}
