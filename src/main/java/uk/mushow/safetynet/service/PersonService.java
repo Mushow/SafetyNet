781b1021f22a69c20766e3bc910b3921b13360b8
Mushow <antoine@mushow.uk>
2024-02-12 19:43:41 +0100

    Add utility methods to determine age and adult status from medical records

--- src/main/java/uk/mushow/safetynet/service/PersonService.java
+++ src/main/java/uk/mushow/safetynet/service/PersonService.java
@@ -2,6 +2,7 @@ package uk.mushow.safetynet.service;
 
 import org.springframework.stereotype.Service;
 import uk.mushow.safetynet.exception.PersonNotFoundException;
+import uk.mushow.safetynet.model.MedicalRecord;
 import uk.mushow.safetynet.model.Person;
 import uk.mushow.safetynet.repository.PersonRepository;
 
@@ -9,9 +10,11 @@ import uk.mushow.safetynet.repository.PersonRepository;
 public class PersonService implements IPersonService {
 
     private final PersonRepository personRepository;
+    private final MedicalRecordService medicalRecordService;
 
-    public PersonService(PersonRepository personRepository) {
+    public PersonService(PersonRepository personRepository, MedicalRecordService medicalRecordService) {
         this.personRepository = personRepository;
+        this.medicalRecordService = medicalRecordService;
     }
 
     @Override
@@ -27,6 +30,15 @@ public class PersonService implements IPersonService {
     @Override
     public void deletePerson(Person person) throws PersonNotFoundException {
         personRepository.delete(person);
+    }
+
+    private Integer getAge(MedicalRecord medicalRecord) {
+        return medicalRecordService.getAge(medicalRecord);
+    }
+
+    public boolean isAdult(String firstName, String lastName) {
+        MedicalRecord medicalRecord = medicalRecordService.getByName(firstName, lastName);
+        return getAge(medicalRecord) >= 18;
     }
 
 }
