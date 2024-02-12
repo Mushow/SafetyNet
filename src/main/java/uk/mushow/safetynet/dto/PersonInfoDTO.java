835a76ae24808ebc08d67c3239ab3c6901b309fb
Mushow <antoine@mushow.uk>
2024-02-13 00:59:35 +0100

    Removed firstName (not used)

--- src/main/java/uk/mushow/safetynet/dto/PersonInfoDTO.java
+++ src/main/java/uk/mushow/safetynet/dto/PersonInfoDTO.java
@@ -1,4 +1,3 @@
 package uk.mushow.safetynet.dto;
 
-public record PersonInfoDTO(String firstName, String lastName, String address,
-                            int age, String email, MedicalInfoDTO medicalInfo) {}
+public record PersonInfoDTO(String lastName, String address, int age, String email, MedicalInfoDTO medicalInfo) {}
