d683882f9f37f737e682b1566331027371e50757
Mushow <antoine@mushow.uk>
2024-02-12 01:35:23 +0100

    Add throws exception

--- src/main/java/uk/mushow/safetynet/service/IFirestationService.java
+++ src/main/java/uk/mushow/safetynet/service/IFirestationService.java
@@ -1,13 +1,14 @@
 package uk.mushow.safetynet.service;
 
+import uk.mushow.safetynet.exception.StationNotFoundException;
 import uk.mushow.safetynet.model.Firestation;
 
 public interface IFirestationService {
 
     void addFirestation(Firestation firestation);
 
-    void updateFirestation(Firestation firestation);
+    void updateFirestation(Firestation firestation) throws StationNotFoundException;
 
-    void deleteFirestation(Firestation firestation);
+    void deleteFirestation(Firestation firestation) throws StationNotFoundException;
 
 }
