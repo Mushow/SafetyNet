1211cb93cf5fc44317b6f08af8d8e6b98c660a43
Mushow <antoine@mushow.uk>
2024-02-12 02:02:45 +0100

    Implement CRUD operations

--- src/main/java/uk/mushow/safetynet/repository/MedicalRecordRepository.java
+++ src/main/java/uk/mushow/safetynet/repository/MedicalRecordRepository.java
@@ -1,4 +1,53 @@
 package uk.mushow.safetynet.repository;
 
+import org.springframework.stereotype.Repository;
+import uk.mushow.safetynet.data.DataWrapper;
+import uk.mushow.safetynet.exception.MedicalRecordNotFoundException;
+import uk.mushow.safetynet.model.MedicalRecord;
+
+@Repository
 public class MedicalRecordRepository {
+
+    private final DataWrapper dataWrapper;
+
+    public MedicalRecordRepository(DataWrapper dataWrapper) {
+        this.dataWrapper = dataWrapper;
+    }
+
+    public void create(MedicalRecord medicalRecord) {
+        dataWrapper.medicalRecords().add(medicalRecord);
+    }
+
+    public void update(MedicalRecord updatedMedicalRecord) throws MedicalRecordNotFoundException {
+        String id = updatedMedicalRecord.getFirstName() + updatedMedicalRecord.getLastName();
+        boolean found = false;
+
+        for (MedicalRecord currentMedicalRecord : dataWrapper.medicalRecords()) {
+            String currentId = currentMedicalRecord.getFirstName() + currentMedicalRecord.getLastName();
+            if (currentId.equals(id)) {
+                updateMedicalRecordInformation(currentMedicalRecord, updatedMedicalRecord);
+                break;
+            }
+        }
+        if (!found) {
+            throw new MedicalRecordNotFoundException("The medical record for: " + updatedMedicalRecord.getFirstName() + " " + updatedMedicalRecord.getLastName() + " was not found!");
+        }
+    }
+
+    public void delete(MedicalRecord medicalRecordToDelete) throws MedicalRecordNotFoundException {
+        String firstName = medicalRecordToDelete.getFirstName();
+        String lastName = medicalRecordToDelete.getLastName();
+        boolean wasRemoved = dataWrapper.medicalRecords().removeIf(medicalRecord ->
+                medicalRecord.getFirstName().equals(firstName) && medicalRecord.getLastName().equals(lastName));
+
+        if (!wasRemoved) {
+            throw new MedicalRecordNotFoundException("The medical record for: " + firstName + " " + lastName + " was not found!");
+        }
+    }
+
+    private void updateMedicalRecordInformation(MedicalRecord current, MedicalRecord update) {
+        current.setBirthdate(update.getBirthdate());
+        current.setMedications(update.getMedications());
+        current.setAllergies(update.getAllergies());
+    }
 }
