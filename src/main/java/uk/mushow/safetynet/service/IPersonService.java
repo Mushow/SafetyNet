a379e1c9fabbd925655096fbe939937dda4f1976
Mushow <antoine@mushow.uk>
2024-02-12 01:36:27 +0100

    Add throws exception signature

--- src/main/java/uk/mushow/safetynet/service/IPersonService.java
+++ src/main/java/uk/mushow/safetynet/service/IPersonService.java
@@ -1,13 +1,14 @@
 package uk.mushow.safetynet.service;
 
+import uk.mushow.safetynet.exception.PersonNotFoundException;
 import uk.mushow.safetynet.model.Person;
 
 public interface IPersonService {
 
     void addPerson(Person person);
 
-    void updatePerson(Person person);
+    void updatePerson(Person person) throws PersonNotFoundException;
 
-    void deletePerson(Person person);
+    void deletePerson(Person person) throws PersonNotFoundException;
 
 }
